7f1dc4c84b07cf03f449ae85c6946cd52a4ae025
MrTea7171 <chapanch98@gmail.com>
2025-03-22 01:24:37 +0530

    add eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,25 @@
+// @ts-check
+
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+    eslint.configs.recommended,
+    tseslint.configs.recommendedTypeChecked,
+    {
+        languageOptions: {
+            parserOptions: {
+                projectService: { allowDefaultProject: ['*.mjs'] },
+                tsconfigRootDir: import.meta.dirname,
+            },
+        },
+
+        rules: {
+            'no-console': 'error',
+            'dot-notation': 'error',
+        },
+    },
+    {
+        ignores: ['node_modules', 'dist'],
+    },
+);
