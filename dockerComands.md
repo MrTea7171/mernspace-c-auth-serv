dbc762df8ea6ebfeb56d39b9aa2623ef1080cc29
MrTea7171 <chaipanch98@gmail.com>
2025-04-02 08:06:00 +0530

    added db test

--- dockerComands.md
+++ dockerComands.md
@@ -1,3 +1,5 @@
 docker run --rm -it -v ${PWD}:/usr/src/app -v /usr/src/app/node_modules --env-file ${PWD}/.env -p 5501:5501 -e NODE_ENV=development auth-service:dev
 
 docker build -t auth-service:dev -f docker/dev/Dockerfile .
+
+docker run --rm --name mernpg -e POSTGRES_USER=root -e POSTGRES_PASSWORD=root -v mernpgdata:/var/lib/postgressql/data -p 5432:5432 -d postgres
--- tests/users/register.spec.ts
+++ tests/users/register.spec.ts
@@ -1,7 +1,23 @@
 import request from 'supertest';
 import app from '../../src/app';
+import { DataSource } from 'typeorm';
+import { AppDataSource } from '../../src/config/data-source';
+import { truncateTables } from '../utlis';
 
 describe('POST /auth/register', () => {
+    let connection: DataSource;
+
+    beforeAll(async () => {
+        connection = await AppDataSource.initialize();
+    });
+    beforeEach(async () => {
+        // Data truncation
+        await truncateTables(connection);
+    });
+    afterAll(async () => {
+        await connection.destroy();
+    });
+
     describe('Given all fields', () => {
         it('should return 201 status code', async () => {
             //Arrange
@@ -46,11 +62,11 @@ describe('POST /auth/register', () => {
                 password: 'password',
             };
             //Act
-            const response = await request(app)
-                .post('/auth/register')
-                .send(userData);
+            await request(app).post('/auth/register').send(userData);
             //Assert
-            expect(response.statusCode).toBe(201);
+            const userRepositiory = connection.getRepository('User');
+            const users = await userRepositiory.find();
+            expect(users.length).toBe(1);
         });
     });
     describe('Field are missing', () => {});
